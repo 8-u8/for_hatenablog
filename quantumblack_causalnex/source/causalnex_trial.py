ba607a1a86acd1f759f017e803f198d785e4495c
8-u8 <itotaka523@gmail.com>
2021-03-27 22:45:12 +0900

    make pkl for diagram

--- quantumblack_causalnex/source/causalnex_trial.py
+++ quantumblack_causalnex/source/causalnex_trial.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import graphviz
 import warnings
+import pickle
 
 from IPython.display import Image
 from sklearn.preprocessing import LabelEncoder
@@ -22,6 +23,7 @@ import gc
 from causalnex.structure import StructureModel
 from causalnex.plots import plot_structure, NODE_STYLE, EDGE_STYLE
 from causalnex.structure.notears import from_pandas
+from causalnex.network import BayesianNetwork
 
 # %%
 # sm = StructureModel()
@@ -78,14 +80,14 @@ no_tears_sm = from_pandas(student_por)
 # does not work. maybe my local PC spec is low
 # or graphviz version is not match.
 # I think it is caused by my PC.
-viz = plot_structure(no_tears_sm,
-                     graph_attributes={"scale": "0.5"},
-                     all_node_attributes=NODE_STYLE.WEAK,
-                     all_edge_attributes=EDGE_STYLE.WEAK)
-# %% view
-filename = './output/structure_model.png'
-viz.draw(filename)
-Image(filename)
+# viz = plot_structure(no_tears_sm,
+#                      graph_attributes={"scale": "0.5"},
+#                      all_node_attributes=NODE_STYLE.WEAK,
+#                      all_edge_attributes=EDGE_STYLE.WEAK)
+# # %% view
+# filename = './output/structure_model.png'
+# viz.draw(filename)
+# Image(filename)
 # %%
 no_tears_sm.remove_edges_below_threshold(0.8)
 viz = plot_structure(no_tears_sm,
@@ -94,4 +96,40 @@ viz = plot_structure(no_tears_sm,
                      all_edge_attributes=EDGE_STYLE.WEAK)
 Image(viz.draw(format='png'))
 # it works.
+# %% modify the relationship
+no_tears_sm = from_pandas(student_por,
+                          tabu_edges=[('higher', 'Medu')],
+                          w_threshold=0.8)
+
+# %% visualization modified structure.
+viz = plot_structure(no_tears_sm,
+                     graph_attributes={"scale": "0.5"},
+                     all_node_attributes=NODE_STYLE.WEAK,
+                     all_edge_attributes=EDGE_STYLE.WEAK)
+Image(viz.draw(format='png'))
+
+# %% add and remove edges by hypothesis or knowledges.
+no_tears_sm.add_edge('failure', 'G1')
+no_tears_sm.remove_edge('Pstatus', 'G1')
+no_tears_sm.remove_edge('address', 'G1')
+
 # %%
+viz = plot_structure(no_tears_sm,
+                     graph_attributes={"scale": "0.5"},
+                     all_node_attributes=NODE_STYLE.WEAK,
+                     all_edge_attributes=EDGE_STYLE.WEAK)
+Image(viz.draw(format='png'))
+
+# %%
+no_tears_sm = no_tears_sm.get_largest_subgraph()
+viz = plot_structure(no_tears_sm,
+                     graph_attributes={"scale": "0.5"},
+                     all_node_attributes=NODE_STYLE.WEAK,
+                     all_edge_attributes=EDGE_STYLE.WEAK)
+Image(viz.draw(format='png'))
+# %% save sm
+filename = '../output/no_tears_sm.pkl'
+pickle.dump(no_tears_sm, open(filename, 'wb'))
+
+# %% causalnex -> bayesiannet
+bn = BayesianNetwork(no_tears_sm)
