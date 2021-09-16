84066a0705220772af8014bfb45323665ab24739
8-u8 <itotaka523@gmail.com>
2021-09-16 22:38:20 +0900

    edited

--- stan_hierarchical_model/source/simulated_glmm_data.R
+++ stan_hierarchical_model/source/simulated_glmm_data.R
@@ -11,13 +11,14 @@ simulate_data <- lme4::mkDataTemplate(formula = form, nGrps = 5 , nPerGrp = 500,
 params <- lme4::mkParsTemplate(formula = form, data = simulate_data)
 params$beta[] <- c(0.1, 0.5, 0.7)
 
-# 変量効果の分散共分散行列をガチャる。
+# 変量効果の分散共分散行列を定義する。
 random_effect <- matrix(c(1.0, 0.3,
                           0.3, 1.0),2,2)
 params$theta[] <- lme4::Vv_to_Cv(lme4::mlist2vec(random_effect))
 
 simulate_data$y <- simulate(form, newdata = simulate_data, newparams = params, family = "gaussian")
 
+# modeling
 model1 <- lm(y$sim_1~X1+X2 ,data = simulate_data)
 summary(model1)
 
@@ -27,7 +28,7 @@ summary(model2)
 model3 <- lme4::lmer(y$sim_1 ~ X1 + X2 + (X2 | grp), data = simulate_data)
 summary(model3)
 
-# model4 <- lme4::lmer(y$sim_1 ~ X1 + X2 + (1 + X2 | grp), data = simulate_data)
+model4 <- lme4::lmer(y$sim_1 ~ X1 + X2 + (1 + X2 | grp), data = simulate_data)
 summary(model4)
 
 anova(model4,model3,model2, model1)
